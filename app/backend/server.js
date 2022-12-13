e9438bbdb9c33389cd6320efeffae94a17c69404
molnarbalazsnandor <molnarbalazsnandor@gmail.com>
2022-12-13 10:13:35 +0100

    backend server

---
+++ app/backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.json());
+
+app.get("/beers", (req, res) => {
+  res.send({ response: "beers" });
+});
+
+app.listen(9000, () => {
+  console.log("http://127.0.0.1:9000");
+});
--- app/src/App.js
+++ app/src/App.js
@@ -5,79 +5,16 @@ import LoadingMask from "./components/LoadingMask";
 
 function App() {
   const [beers, setBeers] = useState([]);
-  const [perPage, setPerPage] = useState(10);
-  const [filter, setFilter] = useState("");
-  const [sortBy, setSortBy] = useState("asc");
 
   useEffect(() => {
-    fetch(`https://api.punkapi.com/v2/beers?per_page=${perPage}`)
+    fetch("/beers")
       .then((res) => res.json())
-      .then((data) => {
-        setTimeout(() => {
-          setBeers(data);
-        }, 2000);
-      });
-  }, [perPage]);
-
-  /* Chrome-ban működő megoldás 
-  useEffect(() => {
-    sortBy === "asc"
-      ? setBeers([...beers].sort((a, b) => (a.name > b.name ? -1 : 1)))
-      : setBeers([...beers].sort((a, b) => (a.name < b.name ? -1 : 1)));
-  }, [sortBy]);
-
-     Firefox-ban működő megoldás:
-
-useEffect(() => {
-    sortBy === "asc"
-      ? setBeers((beers) => beers.sort((a, b) => a.name > b.name))
-      : setBeers((beers) => beers.sort((a, b) => a.name < b.name));
-  }, [sortBy]); */
-
-  useEffect(() => {
-    sortBy === "asc"
-      ? setBeers([...beers].sort((a, b) => (a.name > b.name ? 1 : -1)))
-      : setBeers([...beers].sort((a, b) => (a.name < b.name ? 1 : -1)));
-  }, [sortBy]);
+      .then((data) => setBeers(data));
+  }, []);
 
   console.log(beers);
 
-  return (
-    <div className="App">
-      <input
-        type="number"
-        value={perPage}
-        onChange={(event) => {
-          setPerPage(event.target.value);
-        }}
-      />
-
-      <p>filter:</p>
-      <input
-        type="text"
-        placeholder="filter"
-        value={filter}
-        onChange={(event) => {
-          setFilter(event.target.value);
-        }}
-      />
-
-      <button
-        onClick={() => {
-          sortBy === "asc" ? setSortBy("desc") : setSortBy("asc");
-        }}
-      >
-        Sort by: {sortBy}
-      </button>
-
-      {/* conditional rendering: */}
-      {beers.length > 0 ? (
-        <Beers beers={beers} filter={filter} />
-      ) : (
-        <LoadingMask />
-      )}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
